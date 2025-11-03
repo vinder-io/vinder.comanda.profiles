f08c90e5e5e3101290064a99979b1f03666def82
Richard Garcia <code.richardy@gmail.com>
2025-11-02 23:35:19 -0300

    refactor: update permissions constants for owners and customers

--- Source/Vinder.Comanda.Profiles.CrossCutting/Constants/Permissions.cs
+++ Source/Vinder.Comanda.Profiles.CrossCutting/Constants/Permissions.cs
@@ -3,5 +3,13 @@ namespace Vinder.Comanda.Profiles.CrossCutting.Constants;
 public static class Permissions
 {
     public const string ViewActivities = "permissions.activities.view";
-    public const string AssignAddresses = "permissions.customers.addresses.assign";
+    public const string ViewOwners = "permissions.owners.view";
+    public const string CreateOwners = "permissions.owners.create";
+    public const string EditOwners = "permissions.owners.edit";
+    public const string DeleteOwners = "permissions.owners.delete";
+
+    public const string ViewCustomers = "permissions.customers.view";
+    public const string CreateCustomers = "permissions.customers.create";
+    public const string EditCustomers = "permissions.customers.edit";
+    public const string DeleteCustomers = "permissions.customers.delete";
 }
