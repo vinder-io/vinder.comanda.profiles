ff01e28a019d06078cf62f2e28375e6857bafa8b
Richard Garcia <code.richardy@gmail.com>
2025-11-12 22:00:17 -0300

    fix: set default pagination values for PageSize and PageNumber in FetchOwnersHandler

--- Source/Vinder.Comanda.Profiles.Application/Handlers/Owner/FetchOwnersHandler.cs
+++ Source/Vinder.Comanda.Profiles.Application/Handlers/Owner/FetchOwnersHandler.cs
@@ -26,8 +26,8 @@ public sealed class FetchOwnersHandler(IOwnerRepository repository) :
             Items = [.. owners.Select(owner => OwnerMapper.AsResponse(owner))],
             Total = (int)totalCount,
 
-            PageSize = message.Pagination?.PageSize ?? 0,
-            PageNumber = message.Pagination?.PageNumber ?? 0
+            PageSize = message.Pagination?.PageSize ?? 20,
+            PageNumber = message.Pagination?.PageNumber ?? 1
         };
 
         return Result<PaginationScheme<OwnerScheme>>.Success(pagination);
